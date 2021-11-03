55fe3cfdc9b8f4381c912b264222ea33091bd582
akshay-augustine-sheby <akshay.sheby@bigbinary.com>
2021-11-04 00:43:02 +0530

    Build list of quizzes page

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,14 +1,21 @@
 import React, { useEffect, useState } from "react";
 
+import { either, isEmpty, isNil } from "ramda";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
 import { registerIntercepts, setAuthHeaders } from "apis/axios";
 import { initializeLogger } from "common/logger";
 import Login from "components/Authentication/Login";
+import PrivateRoute from "components/Common/PrivateRoute";
+
+import Dashboard from "./components/Dashboard";
+import { getFromLocalStorage } from "./helpers/storage";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
+  const authToken = getFromLocalStorage("authToken");
+  const isLoggedIn = !either(isNil, isEmpty)(authToken) && authToken !== "null";
 
   useEffect(() => {
     initializeLogger();
@@ -24,7 +31,13 @@ const App = () => {
     <Router>
       <ToastContainer />
       <Switch>
-        <Route exact path="/" component={Login} />
+        <Route exact path="/login" component={Login} />
+        <PrivateRoute
+          path="/"
+          redirectRoute="/login"
+          condition={isLoggedIn}
+          component={Dashboard}
+        />
       </Switch>
     </Router>
   );
---
+++ app/javascript/src/components/NavBar/index.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+import { resetAuthTokens } from "src/apis/axios";
+
+import { getFromLocalStorage, setToLocalStorage } from "helpers/storage";
+
+const NavBar = () => {
+  const userFirstName = getFromLocalStorage("authUserFirstName");
+  const userLastName = getFromLocalStorage("authUserLastName");
+  const handleLogout = () => {
+    try {
+      setToLocalStorage({
+        authToken: null,
+        authEmail: null,
+        userId: null,
+        userFirstName: null,
+        userLastName: null,
+      });
+      resetAuthTokens();
+      window.location.href = "/login";
+    } catch (error) {
+      logger.error(error);
+    }
+  };
+
+  return (
+    <nav className="bg-white shadow">
+      <div className="px-2 mx-auto max-w-7xl sm:px-4 lg:px-8">
+        <div className="flex justify-between h-16">
+          <div className="flex px-2 lg:px-0">
+            <div className="hidden lg:flex">
+              <Link
+                to="/"
+                className="inline-flex items-center px-1 pt-1 mr-3
+                          font-bold text-4xl leading-5"
+              >
+                Quizzy
+              </Link>
+            </div>
+          </div>
+          <div className="flex items-center justify-end gap-x-4">
+            <Link to="/">Reports</Link>
+            <span
+              className="inline-flex items-center px-2 pt-1 text-sm
+              font-regular leading-5 text-bb-gray-600 text-opacity-50
+              transition duration-150 ease-in-out border-b-2
+              border-transparent focus:outline-none
+              focus:text-bb-gray-700"
+            >
+              {`${userFirstName} ${userLastName}`}
+            </span>
+
+            <a
+              onClick={handleLogout}
+              className="inline-flex items-center px-1 pt-1 text-sm
+              font-semibold leading-5 text-bb-gray-600 text-opacity-50
+              transition duration-150 ease-in-out border-b-2
+              border-transparent hover:text-bb-gray-600 focus:outline-none
+              focus:text-bb-gray-700 cursor-pointer"
+            >
+              LogOut
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
