ec621c40c8c9d48661c2c72130e5017ebf37f4f1
akshay-augustine-sheby <akshay.sheby@bigbinary.com>
2021-11-06 19:45:15 +0530

    added functionalities for add question page

--- app/javascript/src/components/Authentication/Form/QuestionForm.jsx
+++ app/javascript/src/components/Authentication/Form/QuestionForm.jsx
@@ -1,13 +1,31 @@
 import React, { useState } from "react";
 
-import { Input, Select } from "@bigbinary/neetoui/v2";
+import { Input, Select, Button } from "@bigbinary/neetoui/v2";
 
-import Button from "components/Button";
+import Button2 from "components/Button";
 import Container from "components/Container";
 
 const QuestionForm = () => {
   const [question, setQuestion] = useState("");
-  const [option, setOption] = useState({ option1: "", option2: "" });
+  const [options1, setOptions1] = useState([{ option: "" }, { option: "" }]);
+
+  const handleChange = (id, e) => {
+    const values = [...options1];
+    values[id].value = e.target.value;
+    setOptions1(values);
+  };
+
+  const handleAdd = () => {
+    const values = [...options1];
+    values.push({ option: "" });
+    setOptions1(values);
+  };
+
+  const handleRemove = id => {
+    const values = [...options1];
+    values.splice(id, 1);
+    setOptions1(values);
+  };
 
   return (
     <Container>
@@ -21,20 +39,47 @@ const QuestionForm = () => {
             type="text"
             onChange={e => setQuestion(e.target.value)}
           />
-          <Input
-            label="Option 1"
-            placeholder=""
-            size="small"
-            type="text"
-            onChange={e => setOption({ ...option, option1: e.target.value })}
-          />
-          <Input
-            label="Option 2"
-            placeholder=""
-            size="small"
-            type="text"
-            onChange={e => setOption({ ...option, option2: e.target.value })}
-          />
+          {options1.map((opt, id) => {
+            if (id <= 1) {
+              return (
+                <div key={`${opt}-${id}`}>
+                  <Input
+                    label={`Option ${id + 1}`}
+                    placeholder=""
+                    size="small"
+                    type="text"
+                    onChange={e => handleChange(id, e)}
+                  />
+                </div>
+              );
+            }
+
+            return (
+              <div key={`${opt}-${id}`}>
+                <Input
+                  label={`Option ${id + 1}`}
+                  placeholder=""
+                  size="small"
+                  type="text"
+                  onChange={e => handleChange(id, e)}
+                />
+                <Button
+                  label="Remove"
+                  onClick={() => handleRemove(id)}
+                  style="secondary"
+                />
+              </div>
+            );
+          })}
+
+          {options1.length <= 3 && (
+            <Button
+              label="+ Add option"
+              onClick={() => handleAdd()}
+              style="link"
+            />
+          )}
+
           <div className="p-4 mb-2">
             <Select
               defaultValue={{
@@ -45,20 +90,17 @@ const QuestionForm = () => {
               isSearchable
               label="Correct answer"
               name="ValueList"
-              options={[
-                {
-                  label: option.option1,
-                  value: option.option1,
-                },
-                {
-                  label: option.option2,
-                  value: option.option2,
-                },
-              ]}
+              options={options1.map(opt => {
+                //console.log(opt)
+                return {
+                  label: opt.value,
+                  value: opt.value,
+                };
+              })}
               placeholder="Select an Option"
             />
           </div>
-          <Button type="submit" buttonText="Submit" />
+          <Button2 type="submit" buttonText="Submit" />
         </form>
       </div>
     </Container>
