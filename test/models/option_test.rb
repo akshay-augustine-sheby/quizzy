bd422f674f34c87073c7abbbc0ad6278f2182206
akshay-augustine-sheby <akshay.sheby@bigbinary.com>
2021-11-08 14:43:12 +0530

    added testcases to test option validations

--- test/models/option_test.rb
+++ test/models/option_test.rb
@@ -4,6 +4,37 @@ require "test_helper"
 
 class OptionTest < ActiveSupport::TestCase
   def setup
-    @option = Option.new(option: )
+    @user = User.create!(
+      first_name: "Sam", last_name: "Smith", email: "sam@example.com", password: "welcome",
+      password_confirmation: "welcome")
+    @quiz = Quiz.create!(quiz_name: "Sample Quiz", user_id: @user.id)
+    @question = Question.new(
+      question: "Sample Question", answer: "optionD", quiz_id: @quiz.id, option1: "optionA", option2: "optionB")
+  end
+
+  def test_option1_should_be_present
+    @question.option1 = ""
+    assert_not @question.valid?
+    assert_includes @question.errors.full_messages, "Option1 can't be blank"
+  end
+
+  def test_option2_should_be_present
+    @question.option2 = ""
+    assert_not @question.valid?
+    assert_includes @question.errors.full_messages, "Option2 can't be blank"
+  end
+
+  def test_atleast_2_options_should_be_present
+    @question.option2 = ""
+    assert_not @question.valid?
+  end
+
+  def test_more_than_4_options_cant_be_present
+    @question.save!
+    @option3 = Option.create!(option: "optionC", question_id: @question.id)
+    @option4 = Option.create!(option: "optionD", question_id: @question.id)
+    @option5 = Option.new(option: "optionE", question_id: @question.id)
+    assert_not @option5.valid?
+    assert_includes @option5.errors.full_messages, "Option can't add more than 4"
   end
 end
