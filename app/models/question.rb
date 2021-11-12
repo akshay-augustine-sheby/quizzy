f1645a9cb2e4caab93a938f133cfa446fd44aca2
akshay-augustine-sheby <akshay.sheby@bigbinary.com>
2021-11-12 23:26:38 +0530

    modified question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -6,21 +6,21 @@ class Question < ApplicationRecord
 
   validates :name, presence: true
   validates :answer, presence: true
-  validate :option_atleast_2
-  validate :option_atmost_4
+  validate :should_have_atleast_2_options
+  validate :can_have_atmost_4_options
 
   accepts_nested_attributes_for :options, reject_if: proc { |attributes|
  attributes["name"].blank? }, update_only: true, allow_destroy: true
 
   private
 
-    def option_atleast_2
+    def should_have_atleast_2_options
       if options.size < 2
         errors.add(:base, "Provide atleast 2 options")
       end
     end
 
-    def option_atmost_4
+    def can_have_atmost_4_options
       if options.size > 4
         errors.add(:base, "Too many options, maximum only 4 options allowed")
       end
